7e1dde066f743f340fd5d1a0baad8185025e75bd
Scott Davis <scott.davis@productops.com>
2019-06-02 21:42:38 -0700

    individual view

--- src/views/Burndown.vue
+++ src/views/Burndown.vue
@@ -1,11 +1,5 @@
 <template>
-  <div class="about">
-    <h1>Have a nice look at the burndown chart </h1>
-  </div>
-</template>
-
-<template>
-<v-layout px-5 pt-5>
+<v-container>
   <v-layout row justify-content-space-between>
         <v-flex xs4 mr-3 ml-1>
           <v-btn block color="secondary" dark>When To Meet</v-btn>
@@ -18,6 +12,48 @@
           <v-btn block color="secondary" dark>Meeting Notes</v-btn>
       </v-flex> 
     </v-layout>
-    
-</v-layout>
+
+    <v-layout xs12 row justify-content-space-between>
+      <div class="chart-card">
+        <v-card>
+         <img class="img" src="../assets/chart.png">
+         <div class="effort">
+         <h3>Number of Effort:</h3>
+          <v-text-field
+            label="enter your effort"
+            single-line
+            outline
+            required
+          ></v-text-field>
+          <v-btn color="warning">Enter</v-btn>
+           <v-btn flat small>Cancel</v-btn>
+          </div>
+      </v-card>
+      </div>
+      
+       
+    </v-layout>
+
+</v-container> 
 </template>
+
+<style scoped>
+.chart-card {
+  display: block;
+  margin-top: auto;
+  margin-bottom: auto;
+  margin-left: auto;
+  margin-right: auto;
+  width: 90%;
+}
+.img {
+  width: 90%;
+}
+.effort {
+  margin-top: 20px;
+  margin-bottom: 100px;
+  margin-left: auto;
+  margin-right: auto;
+  width: 500px;
+}
+</style>
